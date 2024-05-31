6474faaed9f64bd6ca83c00b8ede91617e99629b
Arstan <arsiksad0v.wgod@gmail.com>
2024-06-01 05:39:14 +0600

    second

---
+++ src/ingredients.js
@@ -0,0 +1,11 @@
+import meatImage from './assets/meat.jpg';
+import cheeseImage from './assets/cheese.jpg';
+import saladImage from './assets/salad.jpg';
+import baconImage from './assets/bacon.jpg';
+
+export const INGREDIENTS = [
+  { name: 'Мясо', price: 80, image: meatImage },
+  { name: 'Сыр', price: 50, image: cheeseImage },
+  { name: 'Салат', price: 10, image: saladImage },
+  { name: 'Бекон', price: 60, image: baconImage },
+];
